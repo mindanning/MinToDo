e06dab19d915b6c2eb8ffa85e613014157d31da0
yD <mindanning@gmail.com>
2025-09-22 17:53:41 +0200

    Initial Commit

---
+++ MinToDo/MinToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinToDoApp.swift
+//  MinToDo
+//
+//  Created by Imstudio on 22/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct MinToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
